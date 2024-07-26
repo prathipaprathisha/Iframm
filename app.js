38d6b6e91841d5f1721cd2d64aa796024112f355
prathipaprathisha <38810325+prathipaprathisha@users.noreply.github.com>
2024-07-26 19:56:17 +0530

    Add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const morgan = require('morgan');
+var indexRouter = require('./routes/index.routes');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json({limit: '50mb'}));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+app.use('/', indexRouter);
+
+// app.use(function (req, res, next) {
+//     next(createError(404));
+//   });
+//   require('./utils/socket');
+  // error handler
+//   app.use(function (err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+//     // render the error page
+//     res.status(err.status || 500);
+// });
+
+app.listen(5000,()=>{
+    console.log("Port Run Localhost:5000");
+})
